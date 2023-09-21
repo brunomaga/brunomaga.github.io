0d2c8886c7164123241cedf9b030d173540ed1fd
Bruno Maga <brunomaga@users.noreply.github.com>
2023-09-21 11:22:10 +0700

    Update run.sh

--- assets/GPT-lite-DeepSpeed/run.sh
+++ assets/GPT-lite-DeepSpeed/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # serial, single GPU:
-# python gptlite_deepspeed.py --deepspeed --deepspeed_config gptlite_config_ds.json
+# python train.py --deepspeed --deepspeed_config ds_config.json
 
 # deepspeed, 8 GPUs:
-deepspeed --num_gpus=8 gptlite_deepspeed.py --deepspeed --deepspeed_config gptlite_config_ds.json
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json
