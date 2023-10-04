ea095c4ffd44e6dc4a4da55228640149c2e7314b
Bruno Magalhaes <brunomaga@gmail.com>
2023-10-04 11:44:27 +0100

    updated code

--- assets/GPT-lite-DeepSpeed/run.sh
+++ assets/GPT-lite-DeepSpeed/run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # single node, single GPU:
-# python train.py --deepspeed --deepspeed_config ds_config.json
+python train.py --deepspeed --deepspeed_config ds_config.json
 
-# deepspeed, 1 node, 8 GPUs:
-deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json #--run_memory_estimation_only
+# distributed data parallelism, no ZeRO
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config_ddp.json
 
-# deepspeed, 1 node, 8 GPUs, pipeline parallelism:
-deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json --pipeline --pipeline_spec_layers  #--run_memory_estimation_only
+# deepspeed, 1 node, 8 GPUs, ZeRO-3:
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json
+
+# deepspeed, 1 node, 8 GPUs, ZeRO-3, with CPU offloading:
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config_offload.json
+
+# deepspeed, 1 node, 8 GPUs, pipeline parallelism, ZeRO-1:
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config_pipe.json --pipeline --pipeline_spec_layers 
 
 
