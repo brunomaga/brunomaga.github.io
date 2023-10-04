b54197f46ee4d2aaf21ebf450f064de027e8a881
Bruno Magalhaes <brunomaga@gmail.com>
2023-10-04 10:39:21 +0100

    run.sh

--- assets/GPT-lite-DeepSpeed/run.sh
+++ assets/GPT-lite-DeepSpeed/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# serial, single GPU:
+# single node, single GPU:
 # python train.py --deepspeed --deepspeed_config ds_config.json
 
-# deepspeed, 8 GPUs:
-deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json
+# deepspeed, 1 node, 8 GPUs:
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config.json #--run_memory_estimation_only
+
+# deepspeed, 1 node, 8 GPUs, pipeline parallelism:
+deepspeed --num_gpus=8 train.py --deepspeed --deepspeed_config ds_config_RAM.json --pipeline --pipeline_spec_layers  #--run_memory_estimation_only
+
 
