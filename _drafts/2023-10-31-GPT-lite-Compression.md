00280af456cacc221fb778a8dc6f2849f31ac7c5
Bruno Magalhaes <t-bmagalhaes@microsoft.com>
2023-11-04 17:42:33 +0000

    draft code and text

--- _drafts/2023-10-31-GPT-lite-Compression.md
+++ _drafts/2023-10-31-GPT-lite-Compression.md
@@ -43,7 +43,22 @@ The diagram our [GPT2 model]({{ site.baseurl }}{% post_url  2023-02-28-GPT-lite 
 
 ### Knowledge Distillation
 
+The main argument for using KE is that training a smaller model from a larger one yields better results than training the smaller model alone. There are many claims, but the rationale is that - e.g. in the multi-class classification use case - the soft labels that are output from a larger model are much better indicators of the input than the hard labels usually passed by humans. This means that the small model will learn better as its objective has better/cleaner labels. Quick example: on a 2-class (dog, cat) classifier, an image of a dog-looking cat has `[0,1]` labels when user provided but `[0.4, 0.6]` when passed by a trained model, which represents better the labels distribution. This in practice avoids confusion on the smaller model and saves capacity for the real task as hand (instead of the task of *cleaning* noise). 
+
 When training the smaller model, we will use KL-divergenge as the ~~metric~~ distance to minimize. There is also the claim that Mean Square Error is a better metric for Knowledge Distillation (in [Comparing Kullback-Leibler Divergence and Mean Squared Error Loss
 in Knowledge Distillation](https://arxiv.org/pdf/2105.08919.pdf) ) but we ignore that for now. 
 
+There's also a **Temperature parameter** that scales the teacher and student logits to control the convergeance of the learning. This is better detailed in [Distilling the Knowledge in a Neural Network](https://arxiv.org/abs/1503.02531).
 
+In the [NLLLoss documentation](https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html#torch.nn.NLLLoss): 
+"The input given through a forward call is expected to contain log-probabilities of each class. [...] Obtaining log-probabilities in a neural network is easily achieved by adding a LogSoftmax layer in the last layer of your network. You may use CrossEntropyLoss instead, if you prefer not to add an extra layer.."
+
+When using [CrossEntropyLoss](https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.h), the input is expected to contain the unnormalized logits for each class"
+
+
+Finally, [KL-divergence](https://pytorch.org/docs/stable/generated/torch.nn.functional.kl_div.html) expects an `input` and a `target` argument to be passed as log-probability and o
+
+In our example, for simplccity, we simply train a studetn as a smaller version of the teacher. But in practice, any architecture could be used for student and teacher, as long as the objective would be the same.
+More importantly, this post covers offline distilation where teacher and student are trained separately. You can also load the teacher in memory, train it, then in `eval` mode use it to train a new student model in `train` mode. But this requires both to be loaded in memory, limiting the maximum size of the models. For that reason, I prefer to train a large teacher as base line, dump all lables, then subsquentely load and train a smaller student alone.
+
+However, for faster runtime, you may you prefer instead to load and train both the teacher and teacher in one run, or you may also have a KD loss function that includes a Mean Square Error of an intermediatte layer of the teacher and the studet. If that's of interest, see the [Knowledge distillation tutorial on pytorch](https://pytorch.org/tutorials/beginner/knowledge_distillation_tutorial.html#knowledge-distillation-run).
--- assets/GPT-lite-Compression/offline_distillation.py
+++ assets/GPT-lite-Compression/offline_distillation.py
@@ -9,29 +9,31 @@ sys.path.insert(0, os.path.join('..', 'GPT-lite-DeepSpeed'))
 import gptlite
 import benchmark
 
-label_filename = lambda batch, folder: os.path.join(folder,f"{batch}.pt") 
+label_filename = lambda batch, folder: os.path.join(folder,f"logits_{batch}.pt") 
 
-def training(model, dataloader, distilation=False):
-  # reminder: CrossEntropyLoss = LogSoftmax + NLLLoss
+def training(model, dataloader, teacher_model=False):
+  # reminder: CrossEntropyLoss(x) = NLLLoss(LogSoftmax(x))
+  # CrossEntropyLog expects unnormalized logits; NLLLoss expects log probabilities
   model.train()
   optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
   start_time = time.time()
   for b, (x, label) in enumerate(dataloader):
     optimizer.zero_grad() 
     output = model(x)
-    if distilation:
-      # output = F.log_softmax(output, dim=1)
-      soft_labels = torch.load(label_filename(b)).to(model.device)
-      loss = F.kl_div(output, soft_labels, log_target=False)
-    else:
+    if teacher_model:
       loss = F.cross_entropy(output, label)
+    else:
+      student_log_probs = F.log_softmax(output, dim=-1)
+      teacher_logits = torch.load(label_filename(b)).to(model.device)
+      teacher_probs = F.softmax(teacher_logits, dim=-1)
+      loss = F.kl_div(student_log_probs, teacher_probs, log_target=False)
     loss.backward()
     optimizer.step()
   print(f"{b}:: {loss.item()}")
   print(f"train runtime: {float(time.time() - start_time)} seconds")
 
 
-def inference(model, dataloader, output=False):
+def inference(model, dataloader, output_labels=False):
   model.eval()
   start_time = time.time()
   with torch.no_grad():
@@ -39,59 +41,40 @@ def inference(model, dataloader, output=False):
       output = model(x)
       accuracy = (x.argmax(1)==label).sum()/len(x) 
       print(f"{b}: {accuracy}%")
-      if output:
-        # output = F.log_softmax(output, dim=1)
+      if output_labels:
         torch.save(output, label_filename(b))
   print(f"inference runtime: {float(time.time() - start_time)} seconds")
 
 
-def main(scale_factor=0.8, output_folder="distilation_output", model='gptlite', random_seed=42):
+def main(scale_factor=0.8, output_folder="output", model='gptlite', random_seed=42):
   
-  assert model in ('gptlite', 'benchmark')
+  torch.manual_seed(random_seed) 
   
-  torch.manual_seed(random_seed)  #set random seed
-  criterion = torch.nn.CrossEntropyLoss() #initialize loss function
-
-  # we will force each step to be a complete new run due to mem leak on GPU when
-  # allocating and deallocating models on the same run
-  
-  #offline distilation: output soft labels on first run, use it on second run
-  first_round_distilation = not os.path.exists(output_folder)
-  scale_factor = 1 if first_round_distilation else 0.8
+  #if folder exists: it contains labels from the teacher, we're training student
+  model_is_teacher = not os.path.exists(output_folder)
+  scale_factor = 1.0 if model_is_teacher else 0.8
   os.makedirs(output_folder, exist_ok=True)
 
-  # initialize GPT-lite model and dataset. Teacher model is scaled
-  if model=='gptlite':
+  # initialize GPT-model and dataset. Teacher model will be scaled
+  if model.lower()=='gptlite':
     gptlite.n_layer    = int(gptlite.n_layer*scale_factor)
     gptlite.n_embd     = int(gptlite.n_embd*scale_factor)
     gptlite.n_head     = int(gptlite.n_head*scale_factor)
     gptlite.block_size = int(gptlite.block_size*scale_factor)
     train_dataset, vocab_size = gptlite.get_dataset()
     model = gptlite.get_model(vocab_size)
-  elif model=='benchmark':
+  elif model.lower()=='benchmark':
     W, L = 8192, 3 # wide model
     # W, L = 256, 2048 # deep model
     train_dataset = benchmark.get_dataset(W*scale_factor)
     model = benchmark.get_model(W*scale_factor, L*scale_factor)
-    
-  #first run, fully train model and output soft labels
-  if first_round_distilation:  
-    if model=='benchmark':
-      model = benchmark.get_model()
-    else:
-      model = gptlite.GPTlite(vocab_size=1024)
-    training(model, dataloader, distilation=False)
-    inference(model, dataloader, output=True)
-
-  #second run, load soft labels and train smaller model with it
   else:
-    if model=='benchmark':
-      model = benchmark.BenchmarkModel(*(benchmark_model_W_L*scale_factor))
-    else:
-
-      model = gptlite.GPTlite(vocab_size=1024)
-    training(model, dataloader, distilation=True)
-    inference(model, dataloader, output=False)
+    raise NotImplementedError(f"model {model} not implemented")
+  
+  #first run, fully train model and output soft labels
+  #second run, load soft labels and train smaller model with it
+  training (model, train_dataset, teacher_model = model_is_teacher)
+  inference(model, train_dataset, output_labels = model_is_teacher)
 
   
 if __name__ == "__main__":
