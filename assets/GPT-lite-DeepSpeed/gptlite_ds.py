0666326b90fa62e80638617b9197741d8077a8d4
brunomaga <brunomaga@gmail.com>
2024-01-16 15:05:23 +0100

    code for megatron data parallelism

--- assets/GPT-lite-DeepSpeed/gptlite_ds.py
+++ assets/GPT-lite-DeepSpeed/gptlite_ds.py
@@ -1,104 +1,19 @@
+import os
+import sys
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import deepspeed
 import torch.distributed as dist
-import os
 
-# replicate GPT-3 Small in Table 2.1 in "Language Models are Few-Shot Learners, Brown et al, 2021"
-
-# depth of the network as number of decoder blocks.
-n_layer = 12
-
-# size of the embeddings (d_model)
-n_embd = 768
-
-# number of attention heads in the Multi-Attention mechanism
-n_head = 12
-
-# block size ie max number of training sequence, the $n_{ctx}$ in the paper .
-block_size = 2048
-
-# dropout rate (variable p) for dropout units
-dropout = 0.1
+import deepspeed
+from deepspeed.pipe import PipelineModule, LayerSpec
 
 
-class Head(nn.Module):
-
-  def __init__(self, head_size):
-    super().__init__()
-    self.key   = nn.Linear(n_embd, head_size, bias=False)
-    self.query = nn.Linear(n_embd, head_size, bias=False)
-    self.value = nn.Linear(n_embd, head_size, bias=False)
-    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-    self.dropout = nn.Dropout(dropout) #randomly prevents some tokens from communicating with each other
-
-  def forward(self, x):
-    B,T,C = x.shape
-    k = self.key(x) #shape (B,T, head_size)
-    q = self.query(x) #shape (B,T, head_size)
-    v = self.value(x) #shape (B,T, head_size)
-
-    #compute self-attention scores
-    wei = q @ k.transpose(-2, -1) #shape (B,T, head_size) @ (B,head_size,T) --> (B,T,T)
-    wei *= C**-0.5 #scale by sqrt(d_k) as per paper, so that variance of the wei is 1
-    wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # (B,T,T)
-    wei = F.softmax(wei, dim=-1) # (B, T, T)
-    wei = self.dropout(wei)
-
-    #perform weighted aggregation of values
-    out = wei @ v # (B, T, T) @ (B, T, head_size) --> (B, T, head_size)
-    return out
-
-
-class MultiHeadAttention(nn.Module):
-  """ Multi-head attention as described in the paper. Simply a collection of heads with concatenated outputs."""
-  def __init__(self, num_heads, head_size):
-    super().__init__()
-    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    self.proj  = nn.Linear(n_embd, n_embd)
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, x):
-    out = torch.cat([head(x) for head in self.heads], dim=-1)
-    out = self.proj(out)
-    out = self.dropout(out)
-    return out
-
-class FeedForward(nn.Module):
-  """ the feed forward network (FFN) in the paper"""
-  def __init__(self, n_embd):
-    super().__init__()
-    # Note: in the paper (section 3.3) we have d_{model}=512 and d_{ff}=2048.
-    # Therefore the inner layer is 4 times the size of the embedding layer
-    self.net = nn.Sequential(
-        nn.Linear(n_embd, n_embd*4),
-        nn.ReLU(),
-        nn.Linear(n_embd*4, n_embd),
-        nn.Dropout(dropout)
-      )
-  def forward(self, x):
-    return self.net(x)
-
-
-class Block(nn.Module):
-  """ Transformer block: comunication (attention) followed by computation (FFN) """
-  def __init__(self, n_embd, n_head):
-    # n_embd: embedding dimension
-    # n_heads : the number of heads we'd like to use
-    super().__init__()
-    head_size = n_embd // n_head
-    self.sa = MultiHeadAttention(n_head, head_size)
-    self.ffwd = FeedForward(n_embd)
-    self.ln1 = nn.LayerNorm(n_embd)
-    self.ln2 = nn.LayerNorm(n_embd)
-
-  def forward(self, x):
-    x = x + self.sa(self.ln1(x))
-    x = x + self.ffwd(self.ln2(x))
-    return x
-
-
+#use base GPTlite model from the GPT-lite post
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(current_dir, '..', 'GPT-lite'))
+from gptlite import n_layer, n_embd, n_head, block_size, Block
 
 class GPTlite(nn.Module):
   def __init__(self, vocab_size, activation_checkpoint_interval=0):
@@ -154,7 +69,6 @@ class GPTlite(nn.Module):
       idx = torch.cat([idx, idx_next], dim=-1) # shape (B, T+1)
     return idx
 
-
   def to_layers(self):  
       return [
           lambda idx:
@@ -166,7 +80,8 @@ class GPTlite(nn.Module):
       ]
 
 
-from deepspeed.pipe import PipelineModule, LayerSpec
+################ PIPELINE VERSION ######################
+
 class GPTlitePipeSpec(PipelineModule):
 
   class EmbeddingsSum(nn.Module):
@@ -226,7 +141,9 @@ def load_tiny_shakespeare_data():
   train_data, valid_data = data[:n], data[n:]
   if rank==0: print("Train data encoded", data.shape, train_data.shape, valid_data.shape)
   return train_data, valid_data, vocab_size
-  
+
+################ HELPERS ######################
+
 def get_dataset():
   
   class GPTliteDataset(torch.utils.data.Dataset):
@@ -273,4 +190,5 @@ def get_model(vocab_size, criterion=None, pipeline_num_stages=0, pipeline_spec_l
     model = GPTlite(vocab_size,
               activation_checkpoint_interval=activation_checkpoint_interval)  
   return model
+
 
