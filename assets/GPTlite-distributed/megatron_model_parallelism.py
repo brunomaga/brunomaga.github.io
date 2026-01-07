4a41f7485ce74fcd6af0cf7b9ffa57d633523977
Bruno Maga <brunomaga@users.noreply.github.com>
2026-01-07 01:58:14 +0100

    Update megatron_model_parallelism.py

--- assets/GPTlite-distributed/megatron_model_parallelism.py
+++ assets/GPTlite-distributed/megatron_model_parallelism.py
@@ -16,6 +16,7 @@ if not dist.is_initialized():
   dist.init_process_group()
 
 
+
 class Megatron_f(torch.autograd.Function):
   """ The f function in Figure 3 in Megatron paper """
 
@@ -28,6 +29,7 @@ class Megatron_f(torch.autograd.Function):
   def backward(ctx, gradient):
       dist.all_reduce(gradient, dist.ReduceOp.SUM, group=ctx.mp_comm_group)
       return gradient, None
+
 
 
 class Megatron_g(torch.autograd.Function):
@@ -43,6 +45,40 @@ class Megatron_g(torch.autograd.Function):
       return gradient, None
 
 
+
+class Megatron_FeedForward(nn.Module):
+  """ the feed forward network (FFN) in the paper, with tensor parallelism as in Megatron-LM MLP block"""
+
+  def __init__(self, n_embd, mp_comm_group=None):
+    super().__init__()
+    self.mp_comm_group = mp_comm_group
+
+    #Fig 3a. MLP: splits first GEMM across colums and second GEMM across rows
+    n_embd_mid = n_embd * 4
+    if self.mp_comm_group:
+        n_embd_mid //= dist.get_world_size()
+
+    self.fc1 = nn.Linear(n_embd, n_embd_mid)
+    self.fc2 = nn.Linear(n_embd_mid, n_embd, bias=False)   # <-- no bias here
+    self.fc2_bias = nn.Parameter(torch.zeros(n_embd))      # <-- bias added after all-reduce
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    if self.mp_comm_group:
+        x = Megatron_f.apply(x, self.mp_comm_group)
+
+    y = F.relu(self.fc1(x))
+
+    z = self.fc2(y)  # matmul only (partial)
+
+    if self.mp_comm_group:
+        z = Megatron_g.apply(z, self.mp_comm_group)
+
+    z = z + self.fc2_bias  # <-- bias AFTER all-reduce
+    z = self.dropout(z)
+    return z
+
+
 class Megatron_Head(nn.Module):
   """ the attention block with tensor parallelism as in Megatron-LM paper"""
 
@@ -50,6 +86,7 @@ class Megatron_Head(nn.Module):
     super().__init__()
     self.mp_comm_group = mp_comm_group
     if mp_comm_group:
+        #Fig 3b. Self-attention: splits first GEMM across colums and second GEMM across rows
         head_size //= dist.get_world_size()
 
     self.key   = nn.Linear(n_embd, head_size, bias=False)
@@ -79,40 +116,9 @@ class Megatron_Head(nn.Module):
     wei = F.softmax(wei, dim=-1)
     wei = self.dropout(wei)
 
+    #perform weighted aggregation of values
     out = wei @ v
     return out
-
-
-class Megatron_FeedForward(nn.Module):
-  """ the feed forward network (FFN) in the paper, with tensor parallelism as in Megatron-LM MLP block"""
-
-  def __init__(self, n_embd, mp_comm_group=None):
-    super().__init__()
-    self.mp_comm_group = mp_comm_group
-
-    n_embd_mid = n_embd * 4
-    if self.mp_comm_group:
-        n_embd_mid //= dist.get_world_size()
-
-    self.fc1 = nn.Linear(n_embd, n_embd_mid)
-    self.fc2 = nn.Linear(n_embd_mid, n_embd, bias=False)   # <-- no bias here
-    self.fc2_bias = nn.Parameter(torch.zeros(n_embd))      # <-- bias added after all-reduce
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, x):
-    if self.mp_comm_group:
-        x = Megatron_f.apply(x, self.mp_comm_group)
-
-    y = F.relu(self.fc1(x))
-
-    z = self.fc2(y)  # matmul only (partial)
-
-    if self.mp_comm_group:
-        z = Megatron_g.apply(z, self.mp_comm_group)
-
-    z = z + self.fc2_bias  # <-- bias AFTER all-reduce
-    z = self.dropout(z)
-    return z
 
 
 def get_megatron_tensor_parallel_model(vocab_size, mp_comm_group=None):
